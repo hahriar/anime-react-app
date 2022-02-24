12d4ea95da0dcb469057e99bf3dfda52c6b2c3d2
hahriar <closebrocircle@gmail.com>
2022-02-24 19:44:42 +0600

    Last Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Results></Results>
     </div>
   );
+}
+function Results(){
+  const [results , setResults] = useState([]);
+  useEffect( () =>{
+    fetch('https://api.jikan.moe/v3/search/anime?q=naruto')
+    .then(res => res.json())
+    .then(data => setResults(data.results))
+  }, [])
+  return(
+    <div>
+      <h1>Rest Anime List</h1>
+      <h4> Anime Available: {results.length}</h4>
+      <div  className='container-row'>
+      {
+        results.map(result => <Result title={result.title} cover={result.image_url} rate={result.rated} about={result.synopsis}></Result>)
+      }
+      </div>
+    </div>
+  )
+}
+
+function Result(props){
+  //console.log(props)
+  return(
+    <div>
+      <img src={props.cover} alt="" />
+        <h2> Name: {props.title}</h2>
+        <p>Rating: {props.rate}</p>
+        <p><small>About: {props.about}</small></p>
+        </div>
+
+  )
 }
 
 export default App;
